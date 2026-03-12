5ba4b76104df3176fd69775d97dcdf8f4c8dfda1
YohanCollado <yohancollado@yahoo.com>
2026-03-11 20:55:13 -0400

    Innitiated the file uploade component

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,9 +1,33 @@
-import React from "react";
+'use client'
+
+import React, {useCallback} from 'react'
+import {useDropzone} from 'react-dropzone'
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import { ClassNames } from '@emotion/react';
+import { FaCloudUploadAlt } from "react-icons/fa";
 
 const FileUploader = () => {
-    return (
-        <div> File Uploader</div>
-    );
+        const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()} className="cursor-pointer">
+      <input {...getInputProps()} />
+      <Button 
+        type="button"  
+        className={cn("uploader-button", ClassNames)}>
+        <FaCloudUploadAlt height={24} width={24}/>
+      </Button>
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
+    </div>
+  )
 };
 
 export default FileUploader;
